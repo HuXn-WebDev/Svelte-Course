6378d7d210a71e83cab09e7bcbbff0220abde7d6
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:53:06 -0700

    Collections/Lists

---
+++ 10. Collections/1. Example 1/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    const friends = ['alex', 'bob', 'charlie', 'dave', 'eve'];
+</script>
+
+<div>
+    <h1>Friends List</h1>
+    <ul>
+        {#each friends as friend}
+            <li>{friend}</li>
+        {/each}
+    </ul>
+</div>
+
---
+++ 10. Collections/2. Example 2/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    const movies = [
+        { title: "Inception", year: 2010, },
+        { title: "The Matrix", year: 1999 },
+        { title: "Interstellar", year: 2014, },
+        { title: "The Godfather", year: 1972,  },
+        { title: "Pulp Fiction", year: 1994, }
+    ]
+</script>
+
+
+<section>
+    <h1>Movie List</h1>
+    <ul>
+        {#each movies as movie}
+            <li>{movie.title} ({movie.year})</li>
+        {/each}
+    </ul>
+</section>

71ec8dbadf7fa13b4f43f638ab929198586bf7e0
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:48:25 -0700

    Components

---
+++ 1. Component/1. Basic Component/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    // For JavaScript code (logic, functions, etc.)
+</script>
+
+<style>
+    /* For CSS code (styling) */
+</style>
+
+<!-- Everything else is HTML for (UI) -->
+
---
+++ 1. Component/2. Nested Component/src/components/Sidebar.svelte
@@ -0,0 +1,3 @@
+<aside>
+    This Content is coming from the Sidebar component.
+</aside>

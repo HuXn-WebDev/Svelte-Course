c1eb40d046fe14f343b0a88b46b7af1ee337f327
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:49:44 -0700

    State

---
+++ 4. State/1. Example 1/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = $state(0);
+  let increment = () => count += 1
+  let decrement = () => count -= 1
+</script>
+
+<h1>Count: {count}</h1>
+<button onclick={increment}>+</button>
+<button onclick={decrement}>-</button>
---
+++ 4. State/2. Example 2/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  let name = $state('HuXn');
+</script>
+
+
+<h1>My name is: {name}</h1>
+
+<input type="text" bind:value={name} />

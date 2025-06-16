643e6d9632d795b5c0e336258437e1bc9c764ff4
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:48:50 -0700

    Text Interpolation

---
+++ 2. Text Interpolation/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let name = 'HuXn'
+  let age = 23
+  let location = 'China'
+  let hobby = 'coding'
+  let simpleHTML = `<p>This is a simple <b> HTML paragraph </b>.</p>`;
+</script>
+
+
+<h1>My name is: {name}</h1>
+<h2>My age is: {age}</h2>
+<h3>My location is: {location}</h3>
+<h4>My hobby is: {hobby}</h4>
+<p>{@html simpleHTML}</p>
+
+

edf5d3d9d644d321c44b97ef39e0e066a00e79fc
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:51:49 -0700

    Snippets

---
+++ 7. Snippets/1. Example 1/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	const users = [
+		{ id: 1, name: 'Alice', age: 28, location: 'New York' },
+		{ id: 2, name: 'Bob', age: 34, location: 'London' },
+		{ id: 3, name: 'Charlie', age: 25, location: 'Berlin' }
+	];
+</script>
+
+
+{#snippet userCard(user)}
+	<section>
+		<h3>Name: {user.name}</h3>
+		<p>Age: {user.age}</p>
+		<p>Location: {user.location}</p>
+	</section>
+{/snippet}
+
+{#each users as user}
+	{@render userCard(user)}
+{/each}
+
---
+++ 7. Snippets/2. Example 2/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	const products = [
+		{
+			id: 1,
+			name: 'Product 1',
+			price: 19.99,
+			url: 'https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		},
+		{
+			id: 2,
+			name: 'Product 2',
+			price: 29.99,
+			url: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		},
+		{ 
+			id: 3,
+			name: 'Product 3',
+		 	price: 39.99,
+		 	url: 'https://images.unsplash.com/photo-1541623089466-8e777dd05d70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		}
+	];
+</script>
+
+{#snippet productDisplay(product)}
+	<div>
+		<img width="200px" src="{product.url}" alt={product.name} />
+		<div>
+			<h2>{product.name}</h2>
+			<span>${product.price.toFixed(2)}</span>
+		</div>
+	</div>
+{/snippet}
+
+{#each products as product}
+	{@render productDisplay(product)}
+{/each}

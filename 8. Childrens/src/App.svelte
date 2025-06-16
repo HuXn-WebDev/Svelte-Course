20cd4af5c72bb3d9181159e5a8def0e8e804844b
HuXn WebDev <webdevacademy6@gmail.com>
2025-06-16 07:52:16 -0700

    Childrens

---
+++ 8. Childrens/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+   import Child from "./components/Child.svelte";
+</script>
+
+<!-- Whenever you wanna pass some content (HTML)
+    from parent component, down to the child component,
+    you can use the `children` prop.
+-->
+
+<Child>
+    <h1>Hello World</h1>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</Child>
